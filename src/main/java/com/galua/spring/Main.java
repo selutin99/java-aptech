1a68a995336b4eb576b2a813088280e2322d047b
galua <cool.selutin99@yandex.ru>
2021-11-26 19:21:54 +0400

    Update Main.java

--- src/main/java/com/galua/spring/Main.java
+++ src/main/java/com/galua/spring/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.galua.spring");
+
         TestService testServiceBean = context.getBean(TestService.class);
         System.out.println(testServiceBean.test());
     }
